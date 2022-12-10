993513e632567908706eaeedd6904068b3dfd25a
jpx4 <119746561+jpx4@users.noreply.github.com>
2022-12-10 20:23:53 +0100

    Create mcsmaker.cmd

---
+++ mcs-maker/mcsmaker.cmd
@@ -0,0 +1,81 @@
+@echo off
+
+title MCSMaker - MineCraftServerMaker
+
+mkdir serverfiles
+
+:question
+set /p quest=You downloaded Files / configured server ? (y,N): 
+if %quest%==n goto selver
+if %quest%==N goto selver
+if %quest%==y goto runserver
+if %quest%==Y goto runserver
+
+
+
+
+
+
+
+:selver
+set /p version=Select a version {1.8.9plugins,1.8.9}: 
+if %version%==1.8.9plugins goto downloadplugins
+if %version%==1.8.9 goto downloadnormal
+
+:downloadplugins
+cls
+echo Downloading 1.8.9 Engine With Plugins 
+cd serverfiles
+curl https://ftp.jpx4.ml/mcs-maker/engine-versions/paper-1.8.9.jar -o engine.jar
+cd ..
+cls
+goto conf2
+
+:downloadnormal
+cls
+echo Downloading 1.8.9 Engine
+cd serverfiles
+curl https://ftp.jpx4.ml/mcs-maker/engine-versions/vanillia-1.8.9.jar -o engine.jar
+cd ..
+cls
+goto conf2
+
+
+
+
+
+
+
+
+
+:conf2
+set /p motd=Type the server name: 
+cd serverfiles
+echo motd=%motd%>>server.properties
+echo server-ip=localhost>>server.properties
+echo eula=true>>eula.txt
+cls
+echo configured server.
+echo.
+echo server name = %motd%
+echo server ip = localhost
+cd ..
+pause
+goto runserver
+
+
+
+
+
+
+
+
+:runserver
+echo If you dont have the JDK installed then install here: https://adoptium.net
+echo If you have JDK then click enter . . .
+pause>nul
+cd serverfiles
+echo Running the server!
+java -Xmx2G -Xms2G -jar engine.jar nogui
+pause
+
